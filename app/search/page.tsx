8e9f379c17ddc03ffc2a5d42bf88311ba42436e9
Srikant Pandey <154867795+deltacoder2603@users.noreply.github.com>
2025-05-19 02:53:54 +0530

    Update page.tsx

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -112,7 +112,7 @@ export default function Search() {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.3 }}
         >
-          "{query}"
+          &quot{query}&quot
         </motion.h1>
 
         {/* Sources section */}
